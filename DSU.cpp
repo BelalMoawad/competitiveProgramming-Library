7d9c3850ec65361e9ffdd67fd7714fda31ce9843
BelalMoawad <40965840+BelalMoawad@users.noreply.github.com>
2022-10-08 15:05:13 +0200

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -18,43 +18,47 @@ typedef bitset<70> MASK;
 const int N = 1e5 + 9, MOD = 1e9 + 7, OO = 0x3f3f3f3f3f3f3f3f;
 //\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//\\//|
 
-const int MAXNODES = 1e5 + 5;
-
 int n; // number of nodes
-
+ 
 struct DSU {
-	int groupsize[MAXNODES];
-	int parent[MAXNODES];
-	int numofgroups;
-	DSU() {
-		numofgroups = n;
-		for (int i = 0; i < MAXNODES; ++i) {
-			parent[i] = i;
-			groupsize[i] = 1;
-		}
-	}
-	int findleader(int i) {
-		if (parent[i] == i) return i;
-		return parent[i] = findleader(parent[i]);
-	}
-	bool samegroup(int i, int j) {
-		int l1 = findleader(i);
-		int l2 = findleader(j);
-		return l1 == l2;
-	}
-	void mergegroups(int i, int j) {
-		numofgroups--;
-		int l1 = findleader(i);
-		int l2 = findleader(j);
-		if (groupsize[l1] > groupsize[l2]) {
-			parent[l2] = l1;
-			groupsize[l1] += groupsize[l2];
-		}
-		else {
-			parent[l1] = l2;
-			groupsize[l2] += groupsize[l1];
-		}
-	}
+    vi groupsize, parent, groupmax, groupmin;
+    int numofgroups;
+    DSU() {
+        groupsize = vi(n + 1, 1);
+        groupmin = vi(n + 1);
+        groupmax = vi(n + 1);
+        parent = vi(n + 1);
+        numofgroups = n;
+        iota(groupmax.begin(), groupmax.end(), 0);
+        iota(groupmin.begin(), groupmin.end(), 0);
+        iota(parent.begin(), parent.end(), 0);
+    }
+    int findleader(int i) {
+        if (parent[i] == i) return i;
+        return parent[i] = findleader(parent[i]);
+    }
+    bool samegroup(int i, int j) {
+        return findleader(i) == findleader(j);
+    }
+    void mergegroups(int i, int j) {
+        numofgroups--;
+        int l1 = findleader(i);
+        int l2 = findleader(j);
+        if (groupsize[l1] < groupsize[l2]) swap(l1, l2);
+        parent[l2] = l1;
+        groupmin[l1] = min(groupmin[l1], groupmin[l2]);
+        groupmax[l1] = max(groupmax[l1], groupmax[l2]);
+        groupsize[l1] += groupsize[l2];
+    }
+    int getMin(int val) {
+        return groupmin[findleader(val)];
+    }
+    int getMax(int val) {
+        return groupmax[findleader(val)];
+    }
+    int getSize(int val) {
+        return groupsize[findleader(val)];
+    }
 };
 
 void doWork() {
